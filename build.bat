31e7ca9466d85dc35a6be5124be558048dbaf202
101001000 <kike.delacalle.montilla@gmail.com>
2022-12-08 14:23:54 +0100

    build output set to folder /build

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release . 
+mkdir build
+cd build
+cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release ..\ 
 ninja
-copy _deps\sycl_llvm-src\build\bin\sycl6.dll .\
-copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\OpenImageDenoise.dll .\
-copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\tbb12.dll .\
+copy _deps\sycl_llvm-src\build\bin\sycl6.dll .\bin\
+copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\OpenImageDenoise.dll .\bin\
+copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\tbb12.dll .\bin\
 pause

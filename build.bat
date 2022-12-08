134c77b73554dac886740b99b77cead086e5a0c8
101001000 <kike.delacalle.montilla@gmail.com>
2022-12-08 13:30:51 +0100

    .dll copy from deps

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release . 
 ninja
+copy _deps\sycl_llvm-src\build\bin\sycl6.dll .\
+copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\OpenImageDenoise.dll .\
+copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\tbb12.dll .\
 pause

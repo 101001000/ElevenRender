7b20b5ed4ef210042ffe66c5beec4e8c96d4fa4c
101001000 <kike.delacalle.montilla@gmail.com>
2023-02-13 13:25:37 +0100

    Boost submodule & CMake Boost build

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release ..\ 
+cmake -G "Ninja" -D CMAKE_BUILD_TYPE=Release -DBOOST_ALL_NO_LIB=ON ..\ 
 ninja
 copy _deps\sycl_llvm-src\build\bin\sycl.dll .\bin\
 copy _deps\oidnzip-src\oidn-1.4.3.x64.vc14.windows\bin\OpenImageDenoise.dll .\bin\

6aebb3bf3bb21e38eb02c1f28f1a9961cbdbd318
101001000 <kike.delacalle.montilla@gmail.com>
2023-08-03 12:57:19 +0200

    Logging filepath trimmed

--- src/Logging.h
+++ src/Logging.h
@@ -12,12 +12,34 @@
 #include <Windows.h>
 #include <string>
 
-#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+template <typename T, size_t S>
+inline constexpr size_t get_file_name_offset(const T(&str)[S], size_t i = S - 1)
+{
+    return (str[i] == '/' || str[i] == '\\') ? i + 1 : (i > 0 ? get_file_name_offset(str, i - 1) : 0);
+}
+
+template <typename T>
+inline constexpr size_t get_file_name_offset(T(&str)[1])
+{
+    return 0;
+}
+
+namespace utility {
+
+    template <typename T, T v>
+    struct const_expr_value
+    {
+        static constexpr const T value = v;
+    };
+
+}
+
+#define UTILITY_CONST_EXPR_VALUE(exp) ::utility::const_expr_value<decltype(exp), exp>::value
 
 #define LOG(lvl)\
     BOOST_LOG_STREAM_WITH_PARAMS(::boost::log::trivial::logger::get(),\
         (::boost::log::keywords::severity = ::boost::log::trivial::lvl)) \
-        << ::boost::log::add_value("Line", __LINE__) << ::boost::log::add_value("File", __FILENAME__)
+        << ::boost::log::add_value("Line", __LINE__) << ::boost::log::add_value("File", &__FILE__[UTILITY_CONST_EXPR_VALUE(get_file_name_offset(__FILE__))])
 
 
 BOOST_LOG_ATTRIBUTE_KEYWORD(a_timestamp, "TimeStamp", boost::log::attributes::local_clock::value_type);
